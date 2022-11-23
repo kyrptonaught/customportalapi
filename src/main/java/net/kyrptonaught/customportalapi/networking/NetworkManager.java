7b870affaeb368705c482e3b7d9c2e7571287344
kyrptonaught <anthonybuttillo10@gmail.com>
2022-11-23 00:41:12 -0500

    Remove canSend checks, just send anyway

--- src/main/java/net/kyrptonaught/customportalapi/networking/NetworkManager.java
+++ src/main/java/net/kyrptonaught/customportalapi/networking/NetworkManager.java
@@ -10,10 +10,6 @@ public class NetworkManager implements DedicatedServerModInitializer {
     public static final Identifier SYNC_PORTALS = new Identifier(CustomPortalsMod.MOD_ID, "syncportals");
     public static final Identifier PLACE_PORTAL = new Identifier(CustomPortalsMod.MOD_ID, "placeportal");
 
-    public static boolean doesPlayerHaveMod(ServerPlayerEntity player) {
-        return ServerPlayNetworking.canSend(player, SYNC_PORTALS);
-    }
-
     @Override
     public void onInitializeServer() {
         PortalRegistrySync.registerSyncOnPlayerJoin();
